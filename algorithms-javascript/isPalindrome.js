3a19ca2f71c4f96ce8102ad1273a1881a6d08070
Corvax <vlvete1990@yandex.ru>
2020-02-16 18:14:54 +0300

    adding isPalendrome

--- algorithms-javascript/isPalindrome.js
+++ algorithms-javascript/isPalindrome.js
@@ -1,9 +1,22 @@
-function isPalindrome(str) {
-    
+function isPalindrome(string) {
+    string = string.toLowerCase();
+    var charactersArr = string.split('');
+    var validChharacters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+    var lettersArr = [];
+    charactersArr.forEach(char => {
+        if(validChharacters.indexOf(char) > -1) lettersArr.push(char);
+    });
+    if(lettersArr.join('') === lettersArr.reverse().join('')) return true;
+    else return false;
+    // return false or true
 }
 
 function reverseString(str) {
     return str.split('').reverse().join(''); 
 }
 
-console.log(reverseString("Hello!"));
+// console.log(reverseString("Hello!"));
+
+// console.log(isPalindrome("Madam I'm Adam"));
+console.log(isPalindrome("race car"));
