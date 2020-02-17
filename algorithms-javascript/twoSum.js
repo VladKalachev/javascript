e824883d0632a7e995e9715ff668ddc3297f120d
VladKalachev <vlvete1990@yandex.ru>
2020-02-17 16:49:51 +0300

    adding twoSum

--- algorithms-javascript/twoSum.js
+++ algorithms-javascript/twoSum.js
@@ -1,7 +1,24 @@
 function twoSum(numArray, sum) {
     // return every pair of numbers from 'numArray'
     // that adds up to the 'sum'
+    var pairs = [];
+    var hashtable = [];
+
+    for (let i = 0; i < numArray.length; i++) {
+       var currName = numArray[i];
+       var counterpart = sum - currName;
+       if(hashtable.indexOf(counterpart) !== -1) {
+           pairs.push([currName, counterpart]);
+       }
+       hashtable.push(currName);
+    }
+
+    return pairs;
 }
 
 // numArray = [1,6,4,5,3,3] sum = 7
 // result = [[6,1], [3,4], [3,4]]
+
+// test
+// console.log(twoSum([1,6,4,5,3,3], 7));
+// console.log(twoSum([40, 11, 19, 17, -12], 28));
