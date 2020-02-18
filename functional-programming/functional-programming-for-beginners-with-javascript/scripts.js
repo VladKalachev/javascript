4d18170e1b701f0b0778b8acc2715e3477d6b8aa
VladKalachev <vlvete1990@yandex.ru>
2020-02-18 12:12:18 +0300

    immutable data

---
+++ functional-programming/functional-programming-for-beginners-with-javascript/scripts.js
@@ -0,0 +1,12 @@
+var name = 'Fred';
+let firstName = 'Saly';
+
+// immutable
+const Pi = 3.14;
+
+const meal = {
+    descriptin: 'Breakfast',
+    calories: 400,
+}
+meal.calories = 600;
+console.log(meal.calories);
