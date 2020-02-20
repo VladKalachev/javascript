48fc03c75f16ad57758876e2d92ae4a890a8f0d5
VladKalachev <vlvete1990@yandex.ru>
2020-02-20 12:03:20 +0300

    adding curry and ramda

---
+++ functional-programming/functional-programming-for-beginners-with-javascript/demo.js
@@ -0,0 +1,17 @@
+// 1. перебор
+// 2. найти отношение к 10 
+// 3. сравнивать предыдущий коэффециент с настоящем
+// могут быть отрицательные числа
+
+// Найти ближайщее к 10
+// const array = [1, 5, 30, -1];
+// const res = array.map(numb => {
+//    return numb/10;
+// });
+// console.log(res);
+// function sum(x, y) {
+//     console.log(x > y);
+//     return x > y;
+// }
+
+// res.reduce(sum);
--- functional-programming/functional-programming-for-beginners-with-javascript/function.js
+++ functional-programming/functional-programming-for-beginners-with-javascript/function.js
@@ -6,18 +6,35 @@
 //     return `${greeting} ${name}`;
 // }
 
-function greet(greeting) {
-    return function(name) {
-        return `${greeting} ${name}`;
-    };
-}
+// function greet(greeting) {
+//     return function(name) {
+//         return `${greeting} ${name}`;
+//     };
+// }
 
 // console.log(greet('Good Morning', 'James'));
 // console.log(greet('Good Morning')('James'));
 
 const friends = ['Nate', 'Jim', 'Scott', 'Dean'];
 
-//const friendGreeting = friends.map(name => greet('Good Morning', name));
-const friendGreeting = friends.map(greet('Good Morning'));
+// const friendGreeting = friends.map(name => greet('Good Morning', name));
+// const friendGreeting = friends.map(greet('Good Morning'));
 
-console.log(friendGreeting)
+// console.log(friendGreeting)
+
+/**
+ * Partial Application
+ * Using Function
+ */
+// const afternoonGreet = greet('Good Afternoon');
+
+// const greetNate = afternoonGreet('Nate');
+
+// console.log(greetNate);
+
+/**
+ * Currying ramda
+ */
+
+ const greet = R.curry((greeting, name) => `${greeting} ${name}`);
+ console.log(greet('Good Morning')('James'));
