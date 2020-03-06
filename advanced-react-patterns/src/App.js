6bfa43b24c7776eab3b854aa95974fcd1d399279
VladKalachev <vlvete1990@yandex.ru>
2020-03-06 18:51:29 +0300

    adding template

--- advanced-react-patterns/src/App.js
+++ advanced-react-patterns/src/App.js
@@ -1,15 +1,38 @@
-import React from 'react';
-import Clap from './patterns/01';
-import Clap2 from './patterns/02';
-import './App.css';
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { useMediaQuery } from 'react-responsive'
+import { hot } from 'react-hot-loader'
+import Sidebar from './Sidebar'
+import Body from './Body'
+import { SCREEN_SIZES, media } from './content/StyledContent'
 
-function App() {
+const StyledContainer = styled.div`
+  display: grid;
+  min-height: 100vh;
+
+  ${media.md`
+    grid:
+      'sidebar body'
+      1fr /24% 1fr;
+  `}
+`
+
+const App = () => {
+  const isMediumOrLarger = useMediaQuery({ minWidth: SCREEN_SIZES.md })
+
+  // toggle sidebar display
+  const [showSidebar, setShowSidebar] = useState(false)
+  const isSidebarShown = isMediumOrLarger || (!isMediumOrLarger && showSidebar)
+
   return (
-    <div className="App">
-      <Clap/>
-      <Clap2/>
-    </div>
-  );
+    <StyledContainer>
+      {isSidebarShown && <Sidebar setShowSidebar={setShowSidebar} />}
+      <Body
+        setShowSidebar={setShowSidebar}
+        isMediumOrLarger={isMediumOrLarger}
+      />
+    </StyledContainer>
+  )
 }
 
-export default App;
+export default hot(module)(App)
--- advanced-react-patterns/src/index.js
+++ advanced-react-patterns/src/index.js
@@ -3,8 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createGlobalStyle } from 'styled-components'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+*,
+*:before,
+*:after {
+    box-sizing: border-box;
+}
+
+  body {
+    padding: 0;
+    margin: 0;
+    color: #b2bbc8;
+    font-family: Avenir, Nunito, sans-serif;
+    font-size: 16px;
+    font-weight: 500;
+  }
+
+`
+
+ReactDOM.render(
+    <>
+    <GlobalStyle />
+    <App />
+    Hello
+  </>, 
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
