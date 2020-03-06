83f3743c7ef4ccfe9300247dd76699d48f8aac8a
VladKalachev <vlvete1990@yandex.ru>
2020-03-06 17:42:55 +0300

    adding new repo advanced react patterns

---
+++ advanced-react-patterns/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Clap from './patterns/01';
+import Clap2 from './patterns/02';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Clap/>
+      <Clap2/>
+    </div>
+  );
+}
+
+export default App;
